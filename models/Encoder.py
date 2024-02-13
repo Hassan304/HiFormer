9e86602da92d54789cd9401b3c37ec8b2d74ef4c
Hassan Raza <76471344+Hassan304@users.noreply.github.com>
2024-02-13 16:47:27 +0500

    Update Encoder.py

--- models/Encoder.py
+++ models/Encoder.py
@@ -93,8 +93,8 @@ class SwinTransformer(nn.Module):
     def _init_weights(self, m):
         if isinstance(m, nn.Linear):
             trunc_normal_(m.weight, std=.02)
-            if isinstance(m, nn.Linear) and m.bias is not None:
-                nn.init.constant_(m.bias, 0)
+        if isinstance(m, nn.Linear) and m.bias is not None:
+            nn.init.constant_(m.bias, 0)
         elif isinstance(m, nn.LayerNorm):
             nn.init.constant_(m.bias, 0)
             nn.init.constant_(m.weight, 1.0)
