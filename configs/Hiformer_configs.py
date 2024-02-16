da70afeb4d0cc978e5ba326904e97ee93ac99574
Hassan Raza <76471344+Hassan304@users.noreply.github.com>
2024-02-17 03:02:32 +0500

    Update Hiformer_configs.py

--- configs/Hiformer_configs.py
+++ configs/Hiformer_configs.py
@@ -25,7 +25,7 @@ def get_hiformer_b_configs():
 
     # DLF Configs
     cfg.depth = [[1, 2, 0]]  # Depth for small, middle, large levels (added middle level depth)
-    cfg.num_heads = [6, 9, 12]  # Heads for small, middle, large levels (added middle level heads)
+    cfg.num_heads = [6, 8, 12]  # Heads for small, middle, large levels (added middle level heads)
     cfg.mlp_ratio = [2., 2., 1.]  # Ratios for small, middle, large levels (added middle level ratio)
     cfg.drop_rate = 0.
     cfg.attn_drop_rate = 0.
