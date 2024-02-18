389bd72653bf5a7d7ac3d9eaf32e585e3d98a4cd
Hassan Raza <76471344+Hassan304@users.noreply.github.com>
2024-02-18 22:05:11 +0500

    Update Hiformer.py

--- models/Hiformer.py
+++ models/Hiformer.py
@@ -54,10 +54,10 @@ class HiFormer(nn.Module):
         # Find the maximum height and width to resize all feature maps to match this size
         max_h = max(embed.shape[2] for embed in reshaped_embed)
         max_w = max(embed.shape[3] for embed in reshaped_embed)
-
+        target_size = (max_h, max_w)
         # Resize all embeddings to have the same spatial dimensions
-        resized_embeds = [F.interpolate(embed, size=(max_h, max_w), mode='bilinear', align_corners=False) for embed in reshaped_embed]
-
+        #resized_embeds = [F.interpolate(embed, size=(max_h, max_w), mode='bilinear', align_corners=False) for embed in reshaped_embed]
+        resizeed_embeds = [F.interpolate(e, size=target_size, mode='bilinear', align_corners=False) for e in reshaped_embed]
         # Now you can combine the resized embeddings directly
         combined_features = torch.cat(resized_embeds, dim=1)  # Concatenate along the channel dimension
 
